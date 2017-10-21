8f8dd7ccc0cd5cb5607997fa7c51b05d2b6e83a0
Abiola Ibrahim <abiola89@gmail.com>
2017-10-21 19:26:18 +0100

    add builder

---
+++ builder/builder.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+VERSION=${VERSION:-"0.10.10"}
+
+# caddy
+git clone https://github.com/mholt/caddy -b "v$VERSION" /go/src/github.com/mholt/caddy \
+    && cd /go/src/github.com/mholt/caddy \
+    && git checkout -b "v$VERSION"
+
+# plugin helper
+GOOS=linux GOARCH=amd64 go get -v github.com/abiosoft/caddyplug/caddyplug
+alias caddyplug='GOOS=linux GOARCH=amd64 caddyplug'
+
+# plugins
+for plugin in $(echo $PLUGINS | tr "," " "); do \
+    go get -v $(caddyplug package $plugin); \
+    printf "package caddyhttp\nimport _ \"$(caddyplug package $plugin)\"" > \
+        /go/src/github.com/mholt/caddy/caddyhttp/$plugin.go ; \
+    done
+
+# builder dependency
+git clone https://github.com/caddyserver/builds /go/src/github.com/caddyserver/builds
+
+# build
+cd /go/src/github.com/mholt/caddy/caddy \
+    && git checkout -f \
+    && GOOS=linux GOARCH=amd64 go run build.go -goos=$GOOS -goarch=$GOARCH -goarm=$GOARM \
+    && mv caddy /install
+
