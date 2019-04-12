b4ade9cc66478ab2554c936a8fb10167bc79931a
Abiola Ibrahim <abiola89@gmail.com>
2019-04-13 00:51:08 +0100

    go modules support

--- builder/builder.sh
+++ builder/builder.sh
@@ -4,16 +4,21 @@ VERSION=${VERSION:-"0.11.5"}
 TELEMETRY=${ENABLE_TELEMETRY:-"true"}
 
 # add `v` prefix for version numbers
-[ "$(echo $VERSION | cut -c1)" -ge 0 ] 2>/dev/null && VERSION="v$VERSION"
+checkout_tag=false
+[ "$(echo $VERSION | cut -c1)" -ge 0 ] 2>/dev/null && VERSION="v$VERSION" && checkout_tag=true
 
 # caddy
 git clone https://github.com/mholt/caddy -b "$VERSION" /go/src/github.com/mholt/caddy \
     && cd /go/src/github.com/mholt/caddy \
-    && git checkout -b "$VERSION"
+    && $checkout_tag && git checkout -b "$VERSION"
 
 # plugin helper
 GOOS=linux GOARCH=amd64 go get -v github.com/abiosoft/caddyplug/caddyplug
 alias caddyplug='GOOS=linux GOARCH=amd64 caddyplug'
+
+# check for modules support
+go_mod=false
+[ -f /go/src/github.com/mholt/caddy/go.mod ] && export GO111MODULE=on && go_mod=true
 
 # telemetry
 run_file="/go/src/github.com/mholt/caddy/caddy/caddymain/run.go"
@@ -34,16 +39,16 @@ fi
 
 # plugins
 for plugin in $(echo $PLUGINS | tr "," " "); do \
-    go get -v $(caddyplug package $plugin); \
     printf "package caddyhttp\nimport _ \"$(caddyplug package $plugin)\"" > \
         /go/src/github.com/mholt/caddy/caddyhttp/$plugin.go ; \
-    done
+done
 
 # builder dependency
-git clone https://github.com/caddyserver/builds /go/src/github.com/caddyserver/builds
+$go_mod || git clone https://github.com/caddyserver/builds /go/src/github.com/caddyserver/builds
 
 # build
 cd /go/src/github.com/mholt/caddy/caddy \
+    && go get -v ./... \
     && GOOS=linux GOARCH=amd64 go run build.go -goos=$GOOS -goarch=$GOARCH -goarm=$GOARM \
     && mkdir -p /install \
     && mv caddy /install
